38c4c53430d117fe011dc63d015cbfbda363d5ec
arfchancellor <arfchancellor@gmail.com>
2024-04-28 13:05:57 +0100

    Removed public folder

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ const frameMetadata = getFrameMetadata({
     },
   ],
   image: {
-    src: `${NEXT_PUBLIC_URL}/public/purpleman/baseimage.jpeg`,
+    src: `${NEXT_PUBLIC_URL}/purpleman/baseimage.jpeg`,
     aspectRatio: '1:1',
   },
   postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
